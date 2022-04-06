e85c6249f8f4794bdf440ad6f41eced5e42a2b51
i403318 <realsnowkid@gmail.com>
2022-04-06 22:41:39 +0200

    Fix mapping in ProfileController

--- backend/profile-service/src/main/java/com/gobbledn/profileservice/ProfileController.java
+++ backend/profile-service/src/main/java/com/gobbledn/profileservice/ProfileController.java
@@ -3,6 +3,7 @@ package com.gobbledn.profileservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,7 @@ public class ProfileController {
     @Autowired
     private ProfileService service;
 
-    @RequestMapping("")
+    @GetMapping("")
     public List<Profile> getProfiles() {
         return service.getProfile();
     }
